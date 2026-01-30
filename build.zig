438ee1b17d8e7be63758b30bd99f805eca9a5e04
SmileYik <miskyle@outlook.com>
2026-01-30 13:23:37 +0800

    chore(deps): add zapaste

--- build.zig
+++ build.zig
@@ -41,6 +41,12 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
+    const zapaste = b.dependency("zapaste", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    mod.addImport("zapaste", zapaste.module("zapaste"));
+
     // Here we define an executable. An executable needs to have a root module
     // which needs to expose a `main` function. While we could add a main function
     // to the module defined above, it's sometimes preferable to split business
---
+++ src/api.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+const zapaste = @import("zapaste");
+
+const http = std.http;
+const HttpClient = http.Client;
