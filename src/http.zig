855cf6bf4a865bcd8f53bb175c879cbd495905cb
SmileYik <miskyle@outlook.com>
2026-02-02 09:47:01 +0800

    fix http issue

--- src/http.zig
+++ src/http.zig
@@ -34,7 +34,7 @@ pub inline fn request(
     const uri = try std.Uri.parse(options.url);
     const method = options.method;
     const allocator = options.allocator;
-    const body = options.body orelse .None;
+    var body = options.body orelse .None;
     const authorization: std.http.Client.Request.Headers.Value =
         if (options.authorization) |auth|
             .{ .override = auth }
@@ -46,69 +46,39 @@ pub inline fn request(
     const client = &client_;
     defer client.deinit();
 
-    std.log.debug(
-        \\
-        \\
-        \\==================
-        \\  {s}: '{s}'
-        \\  AUTH: '{s}'
-        \\  Body: '{s}'
-        \\------------------
-    , .{
-        @tagName(method),
-        options.url,
-        options.authorization orelse "",
-        @tagName(body),
+    var allocating = std.Io.Writer.Allocating.init(allocator);
+    defer allocating.deinit();
+
+    const payload, const content_type = switch (body) {
+        .FormData => |*data| .{ try data.getBody(), try data.getContentType() },
+        .Manual => |data| .{ data.body, data.content_type },
+        else => .{ null, null },
+    };
+
+    const result = try client.fetch(.{
+        .method = method,
+        .location = .{ .uri = uri },
+        .payload = payload,
+        .headers = .{
+            .accept_encoding = .{ .override = "" },
+            .authorization = authorization,
+            .content_type = if (content_type) |t| .{ .override = t } else .default,
+        },
+        .keep_alive = false,
+        .response_writer = &allocating.writer,
     });
-
-    var req = try client.request(
-        method,
-        uri,
-        .{
-            .headers = .{
-                .accept_encoding = .{ .override = "" },
-                .authorization = authorization,
-            },
-            .keep_alive = false,
-        },
-    );
-    defer req.deinit();
-
-    // send body
-    switch (body) {
-        .None => {
-            try req.sendBodiless();
-        },
-        .Manual => |b| {
-            req.headers.content_type = .{
-                .override = b.content_type,
-            };
-            try req.sendBodyComplete(b.body);
-        },
-        .FormData => |form_| {
-            var form = form_;
-            req.headers.content_type = .{
-                .override = form.getContentType() catch "",
-            };
-            std.log.debug("****BodyStart\n{s}\n****BodyEnd", .{try form.getBody()});
-            try req.sendBodyComplete(try form.getBody());
-        },
-    }
-
-    // receive
-    var response = try req.receiveHead(&.{});
 
     std.log.debug("Response {d}: {s}", .{
-        @intFromEnum(response.head.status),
-        response.head.status.phrase() orelse "",
+        @intFromEnum(result.status),
+        result.status.phrase() orelse "",
     });
-    if (response.head.status.class() != .success) {
+    if (result.status.class() != .success) {
         var buffer: [4096]u8 = undefined;
         const error_msg = try std.fmt.bufPrint(&buffer,
             \\{{ "code": {d}, "message": "{s}" }}
         , .{
-            @intFromEnum(response.head.status),
-            response.head.status.phrase() orelse "",
+            @intFromEnum(result.status),
+            result.status.phrase() orelse "",
         });
         std.log.debug("Request Failed: {s}", .{error_msg});
         return std.json.parseFromSlice(
@@ -119,32 +89,12 @@ pub inline fn request(
         );
     }
 
-    // header
-    var it = response.head.iterateHeaders();
-    while (it.next()) |header| {
-        std.log.debug("Header: {s}: {s}", .{ header.name, header.value });
-    }
-
-    // body
-    var response_buffer: [4 * 1024]u8 = undefined;
-    var reader = response.reader(&response_buffer);
-    var response_body = try std.ArrayList(u8).initCapacity(allocator, 10 * 1024 * 1024);
-    defer response_body.deinit(allocator);
-
-    while (reader.takeArray(response_buffer.len)) |bytes| {
-        try response_body.appendSlice(allocator, bytes);
-    } else |err| switch (err) {
-        std.io.Reader.Error.EndOfStream => {
-            try response_body.appendSlice(allocator, reader.buffer[0..reader.end]);
-        },
-        else => return err,
-    }
-
-    std.log.debug("Request Successful: {s}", .{response_body.items});
+    const content_body = allocating.written();
+    std.log.debug("Request Successful: {s}", .{content_body});
     return std.json.parseFromSlice(
         T,
         allocator,
-        response_body.items,
+        content_body,
         .{ .ignore_unknown_fields = true, .allocate = .alloc_always },
     );
 }
